6f6a100506016ef485a14eb75bde89fa7370b71c
Ana Paula <ana.guevara.valladares@gmail.com>
2025-07-18 13:32:11 -0500

    Sistema gestion de alumnos

--- python/day2.py
+++ python/day2.py
@@ -49,3 +49,71 @@ diccionario = {
 }
 
 print(high_value(diccionario))
+
+
+def existe(students, clave):
+    return clave in students
+
+
+def add_student(students):
+    name = input('Nombre de estudiante:')
+    if existe(students, name.lower()):
+        print('Ya se encuentra en la lista')
+    else:
+        students[name.lower()] = {'edad': 0, 'notas': []}
+
+
+def add_notes(students):
+    name = input('Nombre de estudiante:').lower()
+    if existe(students, name):
+        notas = [int(input('Ingrese nota:')) for x in range(4)]
+        students[name]['notas'] += notas
+    else:
+        print('No existe el registro')
+    print(students)
+
+
+def average(students):
+    for key in students.keys():
+        notas = students[key]['notas']
+        promedio = sum(notas)/len(notas)
+        print(f"{key.upper()} - Promedio:{promedio}")
+
+
+def show_notes(students):
+    for key in students.keys():
+        notas = students[key]['notas']
+        print(f"{key.upper()} - ", end='')
+        [print(x, end=' ') for x in notas]
+        print()
+
+
+def main():
+    students = dict()
+
+    while True:
+        print('************************************************')
+        print('Bienvenido al sistema de gestión de estudiantes')
+        print('************************************************')
+        print('1.Agregar a alumnos')
+        print('2.Agregar notas')
+        print('3.Calcular promedios')
+        print('4.Mostrar lista de alumnos y notas')
+        print('5.Salir')
+        opcion = int(input('Elija una opción:'))
+        if opcion == 1:
+            add_student(students)
+        elif opcion == 2:
+            add_notes(students)
+
+        elif opcion == 3:
+            average(students)
+        elif opcion == 4:
+            show_notes(students)
+
+        if opcion == 5:
+            print("Hasta luego")
+            break
+
+
+main()
