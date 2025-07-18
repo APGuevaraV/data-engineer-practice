1cf77edd056c7d4b2b34ee371b29e13a6775d709
Ana Paula <ana.guevara.valladares@gmail.com>
2025-07-18 12:15:54 -0500

    practice sql

---
+++ SQL/practice1.sql
@@ -0,0 +1,5 @@
+select * from employees;
+select * from employees where salario > 3000 order by salario desc;
+select * from employees where departamento = 'VENTAS' order by edad desc;
+
+select * from employees where edad between 25 and 35 order by edad desc;
