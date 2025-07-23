68666102ff338bef87c96f1bc0b159a8bffe1336
Ana Paula <ana.guevara.valladares@gmail.com>
2025-07-23 17:16:39 -0500

    subqueries

--- SQL/hacker_rank_two.sql
+++ SQL/hacker_rank_two.sql
@@ -23,3 +23,37 @@ CASE
 END as TRIANGLE_TYPE
 FROM 
 TRIANGLES;
+
+--distancia euclidiana 
+select round(sqrt(power(min(lat_n) - max(lat_n),2) +  power(min(long_w) - max(long_w),2)),4) 
+from station;
+
+select round(
+            abs(min(lat_n) - max(lat_n) ) +  
+            abs( min(long_w) - max(long_w))
+        ,4) 
+from station;
+
+select concat(name,'(',upper(left(occupation,1)),')') from occupations order by name;
+
+select concat('There are a total of',' ',count(*),' ',lower(occupation),'s.' )
+from occupations 
+group by occupation 
+order by count(*) asc,occupation asc;
+
+--subconsultas
+--Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+
+(select city, CHAR_LENGTH(city) as lenght
+from station 
+where CHAR_LENGTH(city) 
+in ( select min(CHAR_LENGTH(city)) from station )
+order by city limit 1)
+
+union
+
+(select city, CHAR_LENGTH(city) as lenght
+from station 
+where CHAR_LENGTH(city) 
+in ( select max(CHAR_LENGTH(city)) from station )
+order by city limit 1)
