6199b1840715385f8a51aebfc061c82c31b825b8
Ana Paula <ana.guevara.valladares@gmail.com>
2025-07-25 19:52:26 -0500

    datalemur practice

--- SQL/datalemur/query.sql
+++ SQL/datalemur/query.sql
@@ -63,3 +63,19 @@ GROUP by sender_id
 ORDER by count(*) desc
 limit 2;
 
+/*
+Write a query to find the top 3 most profitable drugs sold, and 
+how much profit they made. Assume that there are no ties in the 
+profits. Display the result from the highest to the lowest total 
+profit.
+
+Definition:
+
+cogs stands for Cost of Goods Sold which is the direct cost associated
+with producing the drug.
+Total Profit = Total Sales - Cost of Goods Sold
+*/
+
+SELECT drug,(total_sales - cogs) as total_profit FROM pharmacy_sales 
+order by total_profit desc
+limit 3;
