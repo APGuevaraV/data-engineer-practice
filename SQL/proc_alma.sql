ddfedc1c85dee726e67f916fd11af7daa918f9ec
Ana Paula <ana.guevara.valladares@gmail.com>
2025-07-28 15:53:03 -0500

    practice proc almacenados

--- SQL/proc_alma.sql
+++ SQL/proc_alma.sql
@@ -17,3 +17,32 @@ begin
 end //
 delimiter ;
 
+delimiter //
+create procedure ActualizarStock (
+	product_id int,
+    increment_stock int
+    )
+begin
+		declare count_product int;
+		declare stock_qty int ;
+        declare new_stock int ;
+        select count(*) into count_product from productos 
+        where producto_id = product_id ;
+		
+		if count_product > 0 THEN
+			select stock into stock_qty from productos where producto_id = product_id;
+            set new_stock = stock_qty + increment_stock;
+            if new_stock < 0 THEN
+				select 'No se puede reducir el stock por debajo' as status;
+			else
+				update productos set stock = new_stock where producto_id = product_id ;
+				select 'Actualizado correctamente';
+			end if;
+        else 
+			select 'Id de Producto no existe';
+        end if;
+end //
+delimiter ;
+
+call AgregarProducto('dento',4,10);
+call ActualizarStock(2,2);
