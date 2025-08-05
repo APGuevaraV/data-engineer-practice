c41731a013cc1e327958785eeac78172c793128f
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-05 11:27:09 -0500

    except

---
+++ SQL/practice3.sql
@@ -0,0 +1,36 @@
+(select distinct c.nombre
+from clientes c
+join ventas v on c.id_cliente=v.id_cliente
+where v.id_producto=101)
+union
+(select distinct c.nombre
+from clientes c
+join ventas v on c.id_cliente=v.id_cliente
+where v.id_producto=103);
+
+select c.nombre from clientes c 
+join ventas v 
+on c.id_cliente = v.id_cliente
+where v.id_producto = 101
+INTERSECT
+select c.nombre from clientes c 
+join ventas v 
+on c.id_cliente = v.id_cliente
+where v.id_producto = 103;
+
+select c.nombre from clientes c 
+LEFT JOIN  ventas v 
+on c.id_cliente = v.id_cliente;
+
+--except
+SELECT DISTINCT c.nombre
+FROM Clientes c
+JOIN Ventas v ON c.id_cliente = v.id_cliente
+WHERE v.id_producto = 102  
+
+EXCEPT
+
+SELECT DISTINCT c.nombre
+FROM Clientes c
+JOIN Ventas v ON c.id_cliente = v.id_cliente
+WHERE v.id_producto = 101; 
