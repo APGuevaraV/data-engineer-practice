33d6b6f9ec94345f498eb0fb2ed08bdda1987947
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-07 12:32:33 -0500

    carga de csv

---
+++ SQL/carga csv/load.sql
@@ -0,0 +1,51 @@
+CREATE TEMPORARY TABLE empleados_temp(
+    id int,
+    nombre varchar(10),
+    departamento varchar(20),
+    sueldo decimal(10,2)
+)
+
+LOAD DATA INFILE 'empleados.csv'
+INTO TABLE empleados_temp
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+----
+CREATE TABLE empleados(
+    id int,
+    nombre varchar(10),
+    departamento varchar(20),
+    sueldo decimal(10,2),
+    nivel varchar(10)
+);
+
+CREATE TEMPORARY TABLE empleados_tempo(
+    id int,
+    nombre varchar(10),
+    departamento varchar(20),
+    sueldo decimal(10,2)
+);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.4/Uploads/empleados.csv'
+INTO TABLE  empleados_tempo
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+INSERT INTO empleados (id, nombre, departamento, sueldo, nivel)
+SELECT 
+    id,
+    nombre,
+    departamento,
+    sueldo,
+    CASE 
+        WHEN sueldo >= 3500 THEN 'Senior'
+        ELSE 'Junior'
+    END AS nivel
+FROM empleados_temp;
+select * from empleados;
+
+-----
