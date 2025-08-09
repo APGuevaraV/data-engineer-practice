82adb9310eb4b28ee34f51a5882708b7eceddeeb
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-08 19:12:12 -0500

    carga de csv y filtrado de datos

--- python/day_12/read_csv.py
+++ python/day_12/read_csv.py
@@ -3,3 +3,8 @@ import pandas as pd
 df = pd.read_csv('ventas.csv')
 ventas_by_category = df.groupby('categoria')['cantidad'].sum()
 print(ventas_by_category)
+
+df_empleados = pd.read_csv('empleados.csv')
+filtered_df = df_empleados[(df_empleados['departamento']
+                           == 'IT') & (df_empleados['sueldo'] > 3800)]
+print(filtered_df.sort_values(by='sueldo', ascending=False))
