a2851044eeb29b233faf6af9d2bff75ed18ebbe4
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-08 19:50:11 -0500

    porcentaje de asistencia por empleado

--- python/day_12/read_csv.py
+++ python/day_12/read_csv.py
@@ -1,28 +1,38 @@
 import pandas as pd
 
-df = pd.read_csv('ventas.csv')
+df = pd.read_csv('csv/ventas.csv')
 ventas_by_category = df.groupby('categoria')['cantidad'].sum()
 print(ventas_by_category)
 
-df_empleados = pd.read_csv('empleados.csv')
+df_empleados = pd.read_csv('csv/empleados.csv')
 filtered_df = df_empleados[(df_empleados['departamento']
                            == 'IT') & (df_empleados['sueldo'] > 3800)]
-print(filtered_df.sort_values(by='sueldo', ascending=False))
+print(filtered_df.sort_values(by='fecha_ingreso'))
 
 
 # total de ventas por ciudad
-df_pedidos = pd.read_csv('pedidos.csv')
+df_pedidos = pd.read_csv('csv/pedidos.csv')
 df_result = df_pedidos.groupby('ciudad').agg(
-    {
-        'total': 'sum',
-        'ciudad': 'count'
-    }
+
+    total_ventas=('total', 'sum'),
+    cantidad_pedidos=('id_pedido', 'count')
+
 )
 
 print(df_result)
 
 
-df_inventario = pd.read_csv('inventory.csv')
+df_inventario = pd.read_csv('csv/inventory.csv')
 df_inventario['valor_total'] = df_inventario['precio']*df_inventario['stock']
 filtered = df_inventario[df_inventario['valor_total'] > 10000]
 print(filtered)
+
+
+df_asistencia = pd.read_csv('csv/asistencia.csv')
+total_dias = df_asistencia.groupby('empleado')['presente'].count()
+dias_presentes = df_asistencia[df_asistencia['presente'] == 'Sí'].groupby('empleado')[
+    'presente'].count()
+porcentaje_asistencia = (dias_presentes / total_dias *
+                         100).fillna(0).sort_values(ascending=False)
+print("\nPorcentaje de asistencia por empleado:")
+print(porcentaje_asistencia)
