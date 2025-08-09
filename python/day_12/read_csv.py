6a0d746d963b9bb32f1868e85f4a693b74807781
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-08 19:18:12 -0500

    lectura de csv y total de ventas por ciudad y nro de pedidos en cada una

--- python/day_12/read_csv.py
+++ python/day_12/read_csv.py
@@ -8,3 +8,15 @@ df_empleados = pd.read_csv('empleados.csv')
 filtered_df = df_empleados[(df_empleados['departamento']
                            == 'IT') & (df_empleados['sueldo'] > 3800)]
 print(filtered_df.sort_values(by='sueldo', ascending=False))
+
+
+# total de ventas por ciudad
+df_pedidos = pd.read_csv('pedidos.csv')
+df_result = df_pedidos.groupby('ciudad').agg(
+    {
+        'total': 'sum',
+        'ciudad': 'count'
+    }
+)
+
+print(df_result)
