90b1f8370c4311bbe90aec65769a0dc52c35a93b
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-11 17:49:35 -0500

    manipulacion de indices

--- python/day_13/productos.py
+++ python/day_13/productos.py
@@ -24,3 +24,33 @@ df.columns = [
     for col in df.columns
 ]
 print("\nColumnas renombradas:\n", df.columns)
+
+
+# indices
+ordered_fecha = df.set_index('Fecha_ingreso')
+print(ordered_fecha.head())
+ordered_fecha.sort_values(by='Fecha_ingreso', ascending=True)
+
+df['Fecha_ingreso'] = pd.to_datetime(df['Fecha_ingreso'])
+
+# 1. Usar 'Fecha_ingreso' como índice y ordenar
+ordered_fecha = df.set_index('Fecha_ingreso').sort_index()
+print("\nOrdenado por fecha:\n", ordered_fecha.head())
+
+# 2. Slicing de fechas
+slicing = ordered_fecha['2024-06-01':'2024-09-01']
+print("\nFiltrado entre junio y septiembre:\n", slicing)
+
+# 3. Resetear índice y agregar mes
+slicing = slicing.reset_index()
+slicing['mes_ingreso'] = slicing['Fecha_ingreso'].dt.month_name()
+print("\nCon columna mes_ingreso:\n", slicing)
+
+
+# manipular indices
+categoria_muebles = df[(df['categoria'] == 'Muebles') & (df['stock'] > 10)]
+print(categoria_muebles.head().reset_index())
+stock_ordered = categoria_muebles.sort_values(
+    by=['stock', 'precio'], ascending=[False, True])
+stock_ordered.columns = [col.upper() for col in stock_ordered.columns]
+print(stock_ordered.head())
