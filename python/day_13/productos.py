c1cac18c169ecb57d4d26cf0e6221f12517fb776
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-11 17:07:40 -0500

    columnas

--- python/day_13/productos.py
+++ python/day_13/productos.py
@@ -8,3 +8,19 @@ print(filtrado.reset_index())
 print(filtrado.info())
 print(filtrado.describe())
 filtrado_ordered = filtrado.sort_values(by='precio', ascending=False)
+
+# manejo de nan
+filtered_nan = df.sort_values(by='precio', na_position='first')
+print(filtered_nan.reset_index())
+filtered_nan['precio'] = filtered_nan['precio'].fillna(
+    filtered_nan['precio'].mean(), inplace=True)
+print(filtered_nan)
+
+
+# renombrar dinamicamente
+df.columns = [
+    "Fecha_" + col.replace("Fecha", "").strip().lower()
+    if "Fecha" in col else col
+    for col in df.columns
+]
+print("\nColumnas renombradas:\n", df.columns)
