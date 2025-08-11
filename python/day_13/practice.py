6b1996ce8a2c6fa7e6d98aef770240b124fd57af
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-11 12:25:15 -0500

    descripcion y exploración inicial

--- python/day_13/practice.py
+++ python/day_13/practice.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv('csv/inventory.csv')
-print(df)
+# print(df)
+# primera 3 columnas
+print(df.head(3))
+print(df.tail(5))
+print('shape del df')
+print(df.shape)
+# tipos de datos
+print(df.info())
+print(df.describe(include='object'))
