0f3fb27b52775753f379672c61765c4b6608e782
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-12 10:50:26 -0500

    fechas con sql

--- SQL/string/cadenas.sql
+++ SQL/string/cadenas.sql
@@ -5,3 +5,8 @@ select
 substring_index(replace(NombreCompleto,'í','i'),' ',1) as nombre,
 substring_index(replace(NombreCompleto,'í','i'),' ',-1) as apellido
 from empleados;
+
+select 
+datediff(current_Date(),FechaIngreso) as días_en_empresa,
+date_add(FechaIngreso, INTERVAL 3 YEAR)
+from empleados;
