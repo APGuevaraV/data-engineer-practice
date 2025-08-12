bd67eb6ee47d823b065b6e59561a7a3bbcc05c30
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-12 11:20:27 -0500

    nullif

--- SQL/string/cadenas.sql
+++ SQL/string/cadenas.sql
@@ -6,7 +6,22 @@ substring_index(replace(NombreCompleto,'í','i'),' ',1) as nombre,
 substring_index(replace(NombreCompleto,'í','i'),' ',-1) as apellido
 from empleados;
 
+--Crea una columna calculada que muestre cuántos días lleva el empleado en la empresa.
+--Además, calcula la fecha en que cumplirá 3 años desde su ingreso.
 select 
 datediff(current_Date(),FechaIngreso) as días_en_empresa,
 date_add(FechaIngreso, INTERVAL 3 YEAR)
+from empleados;
+
+
+--Muestra el sueldo total de cada empleado sumando sueldo y comisión, 
+--usando ISNULL para que los NULL se consideren 0.
+select Sueldo + IFNULL(Comision,0) as total from empleados;
+
+--Crea una columna llamada BonoExtra que sea:
+select if(Comision >500,'Sí','No') as bono_extra from empleados;
+
+--Crea una consulta que muestre el porcentaje de comisión sobre el sueldo (Comision / Sueldo)
+select round((comision/nullif(sueldo,0))*100,2 )
+as porcentaje_comision 
 from empleados;
