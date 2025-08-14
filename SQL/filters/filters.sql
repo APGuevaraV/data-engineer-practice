008b654bbc1dd4b3eb8c009ca531cf670d6352d1
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-14 16:02:48 -0500

    filtro con any

--- SQL/filters/filters.sql
+++ SQL/filters/filters.sql
@@ -9,3 +9,11 @@ from pedidos p
 where month(p.fecha)=2 
 and year(p.fecha)=2025
 );
+
+--Obtener los clientes cuyo monto más alto de pedido sea mayor
+-- que cualquier precio de productos de la categoría Accesorios.
+select distinct c.nombre 
+from clientes c
+join pedidos p
+on c.id_cliente = p.id_cliente
+where monto > any(select precio from productos where categoria = 'Accesorios');
