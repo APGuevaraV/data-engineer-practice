c441cf2e08ea4a99d39ae892eea53b3915d102a7
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-27 19:06:57 -0500

    calificaciones

--- python/day_19/practice.py
+++ python/day_19/practice.py
@@ -18,3 +18,20 @@ by_month = df.groupby(['region', 'mes']).agg(
 print(by_month)
 vendedor_plus = df.groupby('vendedor')['ventas'].sum()
 print(vendedor_plus.idxmax())
+
+data2 = {
+    "estudiante": ["Ana", "Luis", "Carla", "Pedro", "Ana", "Luis", "Carla",
+                   "Pedro"],
+    "curso": ["Matemáticas", "Matemáticas", "Matemáticas", "Matemáticas",
+              "Historia", "Historia", "Historia", "Historia"],
+    "nota": [15, 12, 18, 14, 17, 16, 19, 11],
+    "grupo": ["A", "A", "B", "B", "A", "A", "B", "B"]
+}
+
+df2 = pd.DataFrame(data2)
+
+prommedio = df2.groupby(['curso', 'grupo'])['nota'].mean()
+print(prommedio)
+por_estudiante = df2.groupby(['curso'])['nota'].idxmax()
+resultado = df2.loc[por_estudiante, ['curso', 'estudiante', 'nota']]
+print(resultado)
