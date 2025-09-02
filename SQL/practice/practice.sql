f7fda2b3011ff920a963e72b76e8614ffba973c0
Ana Paula <ana.guevara.valladares@gmail.com>
2025-09-02 16:19:02 -0500

    lista de estudiantes de Lima

--- SQL/practice/practice.sql
+++ SQL/practice/practice.sql
@@ -4,4 +4,16 @@ inscripciones i
 join cursos c 
 on i.curso_id = c.id
 group by i.curso_id
-order by promedio;
+order by promedio desc;
+
+
+-- Muestra los nombres de los estudiantes junto con los cursos que llevan, 
+-- pero solo aquellos de la ciudad Lima.
+
+select e.nombre, c.nombre from 
+estudiantes e 
+join inscripciones i 
+on e.id = i.estudiante_id
+join cursos c 
+on i.curso_id = c.id
+where e.ciudad = 'Lima';
