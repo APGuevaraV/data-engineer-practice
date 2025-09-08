42f42e517836a6cee706bf1943aa361932105c08
Ana Paula <ana.guevara.valladares@gmail.com>
2025-09-08 18:21:27 -0500

    more than 1 product in 1 order

--- SQL/excercises/queries.sql
+++ SQL/excercises/queries.sql
@@ -13,3 +13,13 @@ from productos p
 join detalleorden deto
 on deto.producto_id = p.id
 group by p.categoria
+
+--Encuentra los clientes que han comprado más de un producto distinto en una misma orden.
+select c.id,c.nombre, count(distinct(deto.producto_id)) as nro 
+from clientes c
+join ordenes o 
+on c.id = o.cliente_id
+join detalleorden deto
+on deto.orden_id = o.id
+group by c.id
+having nro > 1;
