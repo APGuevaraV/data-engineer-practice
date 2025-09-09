93a03eb64b79defa7c1a2bfed601ab96655ac5c9
Ana Paula <ana.guevara.valladares@gmail.com>
2025-09-09 18:30:54 -0500

    validacion de password

---
+++ python/day_21/practice2.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+alumnos = {
+    "Ana": 18,
+    "Luis": 20,
+    "María": 19,
+    "Jorge": 20,
+    "Lucía": 18
+}
+# invertir diccionario
+edades = defaultdict(list)
+for nombre, edad in alumnos.items():
+    edades[edad].append(nombre)
+
+
+print(edades)
+
+
+def max_eight(password):
+    if len(password) >= 8:
+        return True
+    else:
+        return False
+
+
+def contain_upper(password):
+    for letter in password:
+        if 65 >= ord(letter) <= 90:
+            return True
+        else:
+            return False
+
+
+def contain_lower(password):
+    for letter in password:
+        if 97 >= ord(letter) <= 122:
+            return True
+        else:
+            return False
+
+
+def password_validation(password):
+
+    eightcharacters = max_eight(password)
+    contain_uppers = contain_upper(password)
+    contain_lowers = contain_lower(password)
+
+    print('Contraseña Válida:', eightcharacters &
+          contain_uppers & contain_lowers)
+
+
+password = 'Anapaula'
+password_validation(password)
