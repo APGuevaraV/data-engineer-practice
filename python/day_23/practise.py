6b10841d59e794d3a94f6e0443c1fc31f4e5ec17
Ana Paula <ana.guevara.valladares@gmail.com>
2025-09-12 18:11:31 -0500

    columnas calculadas

--- python/day_23/practise.py
+++ python/day_23/practise.py
@@ -12,3 +12,16 @@ df = pd.DataFrame(data)
 df['Total ventas'] = df['Precio'] * df['Cantidad']
 gb_base = df.groupby('Categoría')['Total ventas'].sum()
 print(gb_base)
+
+
+data = {
+    "Estudiante": ["Ana", "Luis", "Carlos", "María", "Elena", "Pedro"],
+    "Matemáticas": [15, 12, 18, 14, 16, 11],
+    "Historia": [13, 17, 15, 16, 12, 14],
+    "Inglés": [16, 14, 19, 13, 18, 15]
+}
+df_estudiantes = pd.DataFrame(data)
+df_estudiantes['Promedio'] = df_estudiantes[[
+    'Matemáticas', 'Historia', 'Inglés']].mean(axis=1)
+df_estudiantes['Aprobado'] = df_estudiantes['Promedio'] >= 14
+print(df_estudiantes)
