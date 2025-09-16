2a9eff21aef0809b3934fb2a75fa91b66492a8c4
Ana Paula <ana.guevara.valladares@gmail.com>
2025-09-16 16:23:01 -0500

    detalles_ventas csv

--- python/day_24/practise.py
+++ python/day_24/practise.py
@@ -28,7 +28,8 @@ df_detalle = pd.DataFrame({
 nuevo_df = pd.merge(df_productos, df_detalle, how='inner', on='ProductoID')
 nuevo_df['total'] = nuevo_df['Cantidad']*nuevo_df['Precio']
 gb_producto = nuevo_df.groupby('ProductoID')['total'].sum()
-# print(gb_producto)
+print(gb_producto)
+gb_producto.to_csv('csv/detalle_ventas.csv', encoding='utf-8')
 
 # Dataset 4: Clientes
 df_clientes = pd.DataFrame({
@@ -57,4 +58,4 @@ df_ventas_before = df_ventas[df_ventas['Año'] < 2024]
 df_ventas_from = df_ventas[df_ventas['Año'] >= 2024]
 
 concatenacion = pd.concat([df_ventas_before, df_ventas_from], axis=0)
-print(concatenacion)
+# print(concatenacion)
