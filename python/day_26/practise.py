65d80a24ae26c657f6e1ae4428c916ecfb506614
Ana Paula <ana.guevara.valladares@gmail.com>
2025-09-17 18:17:12 -0500

    Uso de merge

--- python/day_26/practise.py
+++ python/day_26/practise.py
@@ -15,4 +15,19 @@ df_ventas_resumen = df_ventas[df_ventas['Año'] == 2025].copy()
 df_ventas_resumen.sort_values(
     by=['Empleado', 'Cantidad'], ascending=[True, False], inplace=True)
 df_ventas_resumen.reset_index(inplace=True, drop=True)
-print(df_ventas_resumen)
+# print(df_ventas_resumen)
+
+
+df_objetivos = pd.DataFrame({
+    "Empleado": ["Ana", "Luis", "Marta", "Pedro"],
+    "ObjetivoAnual": [10000, 8000, 12000, 9000]
+})
+
+df_join = pd.merge(
+    df_ventas,
+    df_objetivos,
+    how='left',
+    on='Empleado'
+)
+df_join['TotalVendido'] = df_join['Cantidad']*df_join['PrecioUnitario']
+print(df_join)
