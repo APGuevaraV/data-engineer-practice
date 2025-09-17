acb391faefe288a423ff65fff1c9639c87bc353b
Ana Paula <ana.guevara.valladares@gmail.com>
2025-09-17 18:13:49 -0500

    filtrado de dataframes

---
+++ python/day_26/practise.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+df_ventas = pd.DataFrame({
+    "Empleado": ["Ana", "Luis", "Ana", "Marta", "Luis", "Ana", "Pedro",
+                 "Marta"],
+    "Producto": ["Laptop", "Mouse", "Teclado", "Laptop", "Teclado", "Mouse",
+                 "Laptop", "Teclado"],
+    "Cantidad": [2, 5, 3, 1, 2, 4, 3, 2],
+    "PrecioUnitario": [3000, 100, 200, 3000, 200, 100, 3000, 200],
+    "Año": [2024, 2024, 2025, 2025, 2025, 2024, 2025, 2024]
+})
+
+df_ventas_resumen = df_ventas[df_ventas['Año'] == 2025].copy()
+df_ventas_resumen.sort_values(
+    by=['Empleado', 'Cantidad'], ascending=[True, False], inplace=True)
+df_ventas_resumen.reset_index(inplace=True, drop=True)
+print(df_ventas_resumen)
