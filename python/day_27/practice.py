db907044a15010f355a6e5ccf521de790e6ad1dd
Ana Paula <ana.guevara.valladares@gmail.com>
2025-09-18 13:16:00 -0500

    assign + apply

--- python/day_27/practice.py
+++ python/day_27/practice.py
@@ -39,3 +39,23 @@ df_new = df.assign(
     Bonus=lambda x: x['Ventas']*0.1
 )
 print(df_new.query("Region =='Lima' "))
+
+
+# DATAFRAME 2
+
+df_empleados = pd.DataFrame({
+    'Empleado': ['Ana', 'Luis', 'María', 'Pedro', 'Sofía', 'Jorge'],
+    'Departamento': ['Ventas', 'Ventas', 'IT', 'IT', 'RRHH', 'Ventas'],
+    'Salario': [2500, 3000, 4000, 4200, 2800, 3100],
+    'Edad': [25, 30, 28, 35, 40, 27],
+    'AñoIngreso': [2020, 2018, 2019, 2015, 2010, 2021]
+})
+# print(df_empleados)
+
+
+df_bono = df_empleados.assign(
+    Bono=lambda x: x.apply(
+        lambda row: row['Salario']*0.1 if row['Departamento'] == 'Ventas'
+        else row['Salario']*0.05, axis=1)
+)
+print(df_bono)
