babfa187d05fc15fc7e40263c36e0b050b4a1986
Ana Paula <ana.guevara.valladares@gmail.com>
2025-10-09 20:55:36 -0500

    cube

--- SQL/dmc-practise5.sql
+++ SQL/dmc-practise5.sql
@@ -2,3 +2,8 @@ SELECT Año, Mes,Categoria , SUM(Monto) AS Total
 FROM Ventas
 GROUP BY ROLLUP(Año, Mes,Categoria)
 ORDER BY Año, Mes, Categoria;
+
+SELECT Año, Mes, Categoria, SUM(Monto) AS Total
+FROM Ventas
+GROUP BY CUBE(Año, Mes, Categoria)
+ORDER BY Año, Mes, Categoria;
