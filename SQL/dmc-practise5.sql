f2b4244b4caa49ac07d666bb2336cc2ec34239ab
Ana Paula <ana.guevara.valladares@gmail.com>
2025-10-09 20:56:12 -0500

    grouping sets

--- SQL/dmc-practise5.sql
+++ SQL/dmc-practise5.sql
@@ -7,3 +7,18 @@ SELECT Año, Mes, Categoria, SUM(Monto) AS Total
 FROM Ventas
 GROUP BY CUBE(Año, Mes, Categoria)
 ORDER BY Año, Mes, Categoria;
+
+SELECT grouping_id(Año, Mes, Categoria),
+	   Año, Mes, Categoria, SUM(Monto) AS Total
+FROM Ventas
+GROUP BY GROUPING SETS (
+    --(Año, Mes, Categoria), -- detalle completo
+    (Año, Mes),            -- subtotal mes
+    (Año, Categoria),      -- subtotal categoría por año
+    (Año),                 -- total año
+    (Mes, Categoria),                     -- total general
+	(Mes),
+	(Categoria),
+	()
+)
+ORDER BY 1
