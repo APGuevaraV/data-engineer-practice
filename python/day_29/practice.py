3c7ca49943a8c85ea25334a7329ccf48ff1994db
Ana Paula <ana.guevara.valladares@gmail.com>
2025-10-21 17:10:27 -0500

    filtrado y limpieza de datos

--- python/day_29/practice.py
+++ python/day_29/practice.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 # Dataset
@@ -16,4 +17,26 @@ df = pd.DataFrame(data)
 df_filtered = df[df['categoria'] == 'Ropa']
 gb_df = df_filtered.groupby('vendedor')[['venta']].agg(
     {'venta': 'sum'}).sort_values(by='venta', ascending=False)
-print(gb_df)
+# print(gb_df)
+
+
+data_2 = {
+    'id': [1, 2, 3, 3, 4, 5, 6],
+    'nombre': ['Ana', 'Luis', 'Marcos', 'Marcos', 'Elena', np.nan, 'Sofía'],
+    'edad': [28, np.nan, 35, 35, 40, 30, 22],
+    'salario': [2500, 3000, np.nan, np.nan, 4500, 3100, 2000]
+}
+
+df_employees = pd.DataFrame(data_2)
+df_employees_clean = df_employees.drop_duplicates(
+    subset=['id']).reset_index(drop=True)
+
+df_employees_clean['edad'] = df_employees_clean['edad'].fillna(
+    df_employees_clean['edad'].mean())
+
+df_employees_clean['salario'] = df_employees_clean['salario'].fillna(
+    df_employees_clean['salario'].median())
+
+df_employees_clean_result = df_employees_clean[df_employees_clean['salario']
+                                               > df_employees_clean['salario'].mean()]
+print(df_employees_clean_result)
